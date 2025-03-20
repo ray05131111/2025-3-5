0bee189fb60171f9e855695c80cb76a5e976e4ba
ray05131111 <214211@clvsc.tyc.edu.tw>
2025-03-20 13:32:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def handle_message(event):
         completion = client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
-                {"role": "system", "content": "你是一個程式及數學高手"},
+                {"role": "system", "content": "你是中國人"},
                 {"role": "user", "content": user_message}
             ]
         )
