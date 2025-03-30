5b406af0547bba9e5ea4351e22eb4e0fd940b9be
ray05131111 <214211@clvsc.tyc.edu.tw>
2025-03-30 14:29:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
 LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 OPENAI_KEY = os.getenv("OPENAI_KEY")
-
+print(openai.__version__)
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 line_handler = WebhookHandler(LINE_CHANNEL_SECRET)
 client = OpenAI(api_key=OPENAI_KEY)
